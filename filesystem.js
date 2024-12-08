4f59dc6d1a03bdd948ba964dbb0c725bc4d59db6
user <user@kolyan.kolyan.net>
2024-12-08 12:55:43 +0500

    Create new class and refactor
    Был созданы новый класс ObjectTime, являющийся дочерним классом ObjectDate, со
    своими методами:
    	- now возвращает либо текущее по дате значение, либо по переданному
    	- near возвращает ближайший по значению, либо по заданному

--- filesystem.js
+++ filesystem.js
@@ -1,19 +1,51 @@
+
 const fs = require("fs");
 
 class ObjectJSON {
+	#cacheSearch = new Map();
+
+	_validate(...parametr) {
+		for(let item of parametr){
+			if(item === undefined) throw new Error(`Args is undefind`);
+		}
+	};
+
+	_validateField(field){
+		if(!this._data.some(item => field in item)) throw new Error(`Field "${field}" is not exist`);
+
+	};
+
 	constructor(stringJSON) {
 		this._data = stringJSON;
-	}
+	};
 
 	search(field, value) {
-		const items = Number.isInteger(value)
-			? this._data.filter((item) => item[field] == value)
-			: this._data.filter((item) => new RegExp(value).test(item[field]));
 
-		if (items.length) return items;
+		this._validate(field, value);
+		this._validateField(field);
+	
+		const regexp = new RegExp(value);
+	
+		const items = this._data.filter((item, index) => {
+		  if(this.#cacheSearch.has(index)) return true;
+	
+		  const found = typeof value === 'string' || value instanceof String ? regexp.test(item[field]) : item[field] === value;
+	
+		  if(found) this.#cacheSearch.set(index, item);
+	
+		  return found
+		})
+	
+		if(items.length == 0) throw new Error(`Item was not exist by ${field}:${value}`)
+	
+		return items;
 	}
 
 	sort(field, order = true) {
+
+		this._validate(field, order);
+		this._validateField(field);
+
 		this._data.sort((a, b) => {
 			if (a[field] > b[field]) return 1;
 			if (a[field] < b[field]) return -1;
@@ -21,47 +53,58 @@ class ObjectJSON {
 		});
 
 		return order ? this._data : this._data.reverse();
-	}
+	};
 
 	delete(value, field = "ID") {
-		const item = this.search(field, value)[0];
-		const index = [this._data.indexOf(item)];
+		this.#cacheSearch.clear();
 
-		this._data.splice(index, 1);
-	}
+		this.search(field , value);
+
+		for(let index of this.#cacheSearch.keys()){		
+			this._data.splice(index, 1);
+		}
+
+		return `Item was succesful delete by ${field}:${value}`;
+	};
 
 	add(object) {
+		this.#cacheSearch.clear();
+		this._validate(object);
+
 		object.ID = this._data.length;
 		this._data.push(object);
-	}
+	};
 
-	edit(oldValue, newValue, field = "ID") {
-		const item = this.search(field, oldValue)[0];
-		const index = [this._data.indexOf(item)];
+	edit(oldValue, newValue, field) {
+		this.#cacheSearch.clear();
 
-		this._data[index] = newValue;
-	}
+		this._validate(oldValue, newValue, field)	
+		this.search(oldValue, field);
+
+		for(let keys of this.#cacheSearch.keys()){		
+			this._data[keys][field] = newValue;
+		}
+
+		return `Item was succesful edit by ${field}:${value}`;
+	};
 }
 
 class ObjectDate extends ObjectJSON {
+	#formatter = new Intl.DateTimeFormat('ru', {
+		month: '2-digit',
+		day: "2-digit"
+	});
+
 	sort(field, order = true, byDate) {
-		if (!(byDate === null)) return super.sort(field, order);
+		if ((byDate === undefined)) return super.sort(field, order);
+
+		this._validate(field, order, byDate);
+		this._validateField(field);
 
 		this._data.sort((a, b) => {
-			const timeA = new Date(a[field]);
-			const timeB = new Date(b[field]);
-
-			const [dayA, monthA, yearA] = [
-				timeA.getDay(),
-				timeA.getMonth(),
-				timeA.getFullYear(),
-			];
-
-			const [dayB, monthB, yearB] = [
-				timeB.getDay(),
-				timeB.getMonth(),
-				timeB.getFullYear(),
-			];
+			
+			const [dayA, monthA, yearA] = a[field].split('.');
+			const [dayB, monthB, yearB] = b[field].split('.');
 
 			switch (byDate) {
 				case 1:
@@ -70,17 +113,48 @@ class ObjectDate extends ObjectJSON {
 					return monthA - monthB;
 				case -1:
 					return yearA - yearB;
+				default: 
+					throw new Error(`Cannot sort something wrong with date`)
 			}
 		});
 
 		return order ? this._data : this._data.reverse();
+	};
+
+	now(date = undefined) {
+		return date ? super.search("Date", date) : 
+		 super.search("Date", this.#formatter.format(new Date()));
+	};
+}
+
+class ObjectTime extends ObjectJSON{
+	#formatter = new Intl.DateTimeFormat('ru', {
+		hour: 'numeric',
+		minute: 'numeric',
+	})
+
+	now(time, [start, end]) {
+		time = time ? time : this.#formatter.format(Date.now());
+
+		this._validate(time,start,end);
+		this._validateField(start, end);
+
+		const found = this._data.find((item) => item[start] <= time && time <= item[end])
+
+		if(!found) throw new Error(`Item was not found by fields [${start},${end}] with value ${time}`);
+
+		return found;
 	}
 
-	date(date) {
-		let time = date ? new Date(date) : new Date();
-		return new Promise((resolve, reject) => {
-			resolve(super.search("Date", time.toISOString().split("T")[0]));
-			reject();
-		});
+	near(time, start){
+		this._validate(time,start);
+		this._validateField(start);	
+		this.sort(start)
+
+		const found = this._data.find((item) => item[start] > time)
+
+		if(!found) throw new Error(`Item was not near found by fields [${start}] with value ${time}`);
+
+		return found;
 	}
 }
