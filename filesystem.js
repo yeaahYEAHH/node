1704f9b4d7c287b1c6e45aefa1340d55d07490b7
monke <ramensoda228@gmail.com>
2024-12-17 15:22:11 +0500

    Refactoring base(parent) class
    Был произведен рефакторинг базового родительского класса
    с исправдение и оптимизацией новых методов, даже скорее
    полное преобразование класса ObjectJSON в FileJSON

--- filesystem.js
+++ filesystem.js
@@ -1,160 +1,137 @@
-
+const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
-class ObjectJSON {
-	#cacheSearch = new Map();
+function timeStamp() {
+	return new Intl.DateTimeFormat("ru", {
+		timeZone: "Asia/Yekaterinburg",
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+	}).format(new Date());
+}
+
+class FileJSON {
+	#cacheSearch = new WeakMap();
+	#cacheReg = new WeakMap();
+	#cacheSort = new WeakMap();
 
 	_validate(...parametr) {
-		for(let item of parametr){
-			if(item === undefined) throw new Error(`Args is undefind`);
+		if (!this.data)
+			throw new Error(
+				`JSON data not loaded from ${path.basename(this.path)}`
+			);
+
+		for (let item of parametr)
+			if (item === undefined) throw new Error(`Args is undefind`);
+	}
+
+	_validateField(field) {
+		if (!this.data.some((item) => field in item))
+			throw new Error(`Field "${field}" is not exist`);
+	}
+
+	constructor(file) {
+		this.path = file;
+		this.data = null;
+	}
+
+	async load() {
+		try {
+			await fs.promises.access(this.path, fs.constants.F_OK);
+		} catch (err) {
+			throw new Error(`File not found: ${this.path}`);
 		}
-	};
 
-	_validateField(field){
-		if(!this._data.some(item => field in item)) throw new Error(`Field "${field}" is not exist`);
+		try {
+			const data = await fs.promises.readFile(this.path, "utf-8");
+			try {
+				this.data = data;
+			} catch (err) {
+				throw new Error(`Failed to parse JSON-data`);
+			}
+		} catch (err) {
+			throw new Error(`Failed to load JSON file from ${path.basename(this.path)}`);
+		}
 
-	};
-
-	constructor(stringJSON) {
-		this._data = stringJSON;
-	};
+	}
 
 	search(field, value) {
-
 		this._validate(field, value);
 		this._validateField(field);
-	
-		const regexp = new RegExp(value);
-	
-		const items = this._data.filter((item, index) => {
-		  if(this.#cacheSearch.has(index)) return true;
-	
-		  const found = typeof value === 'string' || value instanceof String ? regexp.test(item[field]) : item[field] === value;
-	
-		  if(found) this.#cacheSearch.set(index, item);
-	
-		  return found
-		})
-	
-		if(items.length == 0) throw new Error(`Item was not exist by ${field}:${value}`)
-	
+
+		const regexp = this.#cacheReg.has(value)
+			? this.#cacheSearch.get(value)
+			: this.#cacheSearch.set(value, new RegExp(value));
+
+		const cacheLocal = new Map();
+
+		const items = this.data.filter((item, index) => {
+			if (cacheLocal.has(index)) return true;
+
+			const found =
+				typeof value === "string" || value instanceof String
+					? regexp.test(item[field])
+					: item[field] === value;
+
+			if (found) cacheLocal.set(index, item);
+
+			return found;
+		});
+
+		if (items.length == 0)
+			throw new Error(`Item was not exist by ${field}:${value}`);
+
+		this.#cacheReg.set(value, new RegExp(value));
+
+		for (const [index, item] of cacheLocal)
+			this.#cacheSearch.set(item, index);
+
 		return items;
 	}
 
 	sort(field, order = true) {
-
 		this._validate(field, order);
 		this._validateField(field);
 
-		this._data.sort((a, b) => {
-			if (a[field] > b[field]) return 1;
-			if (a[field] < b[field]) return -1;
+		const cacheKeySort = field + (order ? "_desc" : "_asc");
+
+		if (this.#cacheSort.has(cacheKeySort))
+			return this.#cacheSort.get(cacheKeySort);
+
+		const sortedData = [...this.data].sort((a, b) => {
+			if (a[field] > b[field]) return order ? 1 : -1;
+			if (a[field] < b[field]) return order ? -1 : 1;
 			return 0;
 		});
 
-		return order ? this._data : this._data.reverse();
-	};
+		this.#cacheSort.set(cacheKeySort, sortedData);
+
+		console.log();
+		return sortedData;
+	}
 
 	delete(value, field = "ID") {
-		this.#cacheSearch.clear();
+		this.search(field, value);
 
-		this.search(field , value);
-
-		for(let index of this.#cacheSearch.keys()){		
-			this._data.splice(index, 1);
+		for (let index of this.#cacheSearch.keys()) {
+			this.data.splice(index, 1);
 		}
 
 		return `Item was succesful delete by ${field}:${value}`;
-	};
-
-	add(object) {
-		this.#cacheSearch.clear();
-		this._validate(object);
-
-		object.ID = this._data.length;
-		this._data.push(object);
-	};
-
-	edit(oldValue, newValue, field) {
-		this.#cacheSearch.clear();
-
-		this._validate(oldValue, newValue, field)	
-		this.search(oldValue, field);
-
-		for(let keys of this.#cacheSearch.keys()){		
-			this._data[keys][field] = newValue;
-		}
-
-		return `Item was succesful edit by ${field}:${value}`;
-	};
-}
-
-class ObjectDate extends ObjectJSON {
-	#formatter = new Intl.DateTimeFormat('ru', {
-		month: '2-digit',
-		day: "2-digit"
-	});
-
-	sort(field, order = true, byDate) {
-		if ((byDate === undefined)) return super.sort(field, order);
-
-		this._validate(field, order, byDate);
-		this._validateField(field);
-
-		this._data.sort((a, b) => {
-			
-			const [dayA, monthA, yearA] = a[field].split('.');
-			const [dayB, monthB, yearB] = b[field].split('.');
-
-			switch (byDate) {
-				case 1:
-					return dayA - dayB;
-				case 0:
-					return monthA - monthB;
-				case -1:
-					return yearA - yearB;
-				default: 
-					throw new Error(`Cannot sort something wrong with date`)
-			}
-		});
-
-		return order ? this._data : this._data.reverse();
-	};
-
-	now(date = undefined) {
-		return date ? super.search("Date", date) : 
-		 super.search("Date", this.#formatter.format(new Date()));
-	};
-}
-
-class ObjectTime extends ObjectJSON{
-	#formatter = new Intl.DateTimeFormat('ru', {
-		hour: 'numeric',
-		minute: 'numeric',
-	})
-
-	now(time, [start, end]) {
-		time = time ? time : this.#formatter.format(Date.now());
-
-		this._validate(time,start,end);
-		this._validateField(start, end);
-
-		const found = this._data.find((item) => item[start] <= time && time <= item[end])
-
-		if(!found) throw new Error(`Item was not found by fields [${start},${end}] with value ${time}`);
-
-		return found;
 	}
 
-	near(time, start){
-		this._validate(time,start);
-		this._validateField(start);	
-		this.sort(start)
+	add(object) {
+		this._validate(object);
 
-		const found = this._data.find((item) => item[start] > time)
+		object.ID = Math.floor(
+			(new Date().getTime() / 100_000_000) * Math.random()
+		);
+		this.data.push(object);
 
-		if(!found) throw new Error(`Item was not near found by fields [${start}] with value ${time}`);
-
-		return found;
+		return `Item was succesful add ID:${object.ID}`;
 	}
 }
